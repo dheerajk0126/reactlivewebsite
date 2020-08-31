1bb76166f4be1569ef71e6fe2b091626c91ec8ba
Kumar <dheerajk@dxc.com>
2020-08-31 12:49:26 +0530

    Updates

---
+++ precache-manifest.abe35ab10b513490ea1613f59050a988.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47e4f59a8fa757f5a4eea8fb21376b0",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "610df2b9c0f195a6ff34",
+    "url": "/reactlivewebsite/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e376d263fc053346a414",
+    "url": "/reactlivewebsite/static/css/main.3b7c1546.chunk.css"
+  },
+  {
+    "revision": "610df2b9c0f195a6ff34",
+    "url": "/reactlivewebsite/static/js/2.f3e4c30d.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/reactlivewebsite/static/js/2.f3e4c30d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e376d263fc053346a414",
+    "url": "/reactlivewebsite/static/js/main.18492d42.chunk.js"
+  },
+  {
+    "revision": "479d0dccb2f89142f4fb",
+    "url": "/reactlivewebsite/static/js/runtime-main.2a28c33f.js"
+  },
+  {
+    "revision": "ed3e69328073eddb019653925da99447",
+    "url": "/reactlivewebsite/static/media/Robots-Square.ed3e6932.jpg"
+  },
+  {
+    "revision": "ace8597eca9486059fe31010355efe1d",
+    "url": "/reactlivewebsite/static/media/digital.ace8597e.jpg"
+  },
+  {
+    "revision": "f045094518e2876301841a9a3ca7dd21",
+    "url": "/reactlivewebsite/static/media/marketing.f0450945.jpg"
+  },
+  {
+    "revision": "3d2ea5535f57873c405c6edfd3ba8f55",
+    "url": "/reactlivewebsite/static/media/software.3d2ea553.jpeg"
+  }
+]);
